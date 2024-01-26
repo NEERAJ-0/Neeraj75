3d0f9e6bd1d31ac67ca0a42b5b8c3aa42f763944
Neeraj <u.neerajkumar5302@gmail.com>
2024-01-26 16:18:06 +0530

    new files added

--- pattern5.py
+++ pattern5.py
@@ -1,8 +1,8 @@
 r=int(input('enter no of rows:'))
-temp=r//2
+
 for i in range(r):
     for j in range(r):
-        if i in [0,r-1,j,r-j-1,temp] or j in [temp]:
+        if i in [0,r-1,] or j in [0,r-1]:
             print('*',end=' ')
         else:
             print(' ',end=' ')
--- pattern7.py
+++ pattern7.py
@@ -2,9 +2,9 @@ r=int(input('enter no of rows:'))
 temp=r//2
 for i in range(r):
     for j in range(r):
-        if i in [0,r-1,j,r-j-1,temp] or j in [temp]:
+        if i in [0,r-1,j,r-j-1,temp] or j in [0,r-1,temp]:
             print('*',end=' ')
         else:
-            print(' ',end=' ')
+            print(' ',end='')
     print()
 
